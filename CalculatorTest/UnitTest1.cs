a96bc99a0f0e73c5efdd435a01164cbf8a97d30f
Elsti <Elsti.Shehati@hotmail.it>
2023-04-03 17:31:24 +0200

    Tests fixed

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -10,24 +10,24 @@ namespace CalculatorTest
         }
 
         [Test]
-        [TestCase(1.5f, 3.14f)]
+        [TestCase(1.5f, 3.1f)]
         public void AddTest(float num1, float num2)
         {
-            Assert.DoesNotThrow(() => Calculator.Add(num1, num2));
+            Assert.AreEqual(4.6f, Calculator.Add(num1, num2));
         }
 
         [Test]
         [TestCase(5.5f, 3.1f)]
         public void SubtractTest(float num1, float num2)
         {
-            Assert.DoesNotThrow(() => Calculator.Subtract(num1, num2));
+            Assert.AreEqual(2.4f, Calculator.Substract(num1, num2));
         }
 
         [Test]
         [TestCase(4.5f, 1.5f)]
         public void DivideTest(float num1, float num2)
         {
-            Assert.DoesNotThrow(() => Calculator.Divide(num1, num2));
+            Assert.AreEqual(3f, Calculator.Divide(num1, num2));
         }
 
         [Test]
@@ -41,7 +41,7 @@ namespace CalculatorTest
         [TestCase(2.5f, 3f)]
         public void MultiplyTest(float num1, float num2)
         {
-            Assert.DoesNotThrow(() => Calculator.Multiply(num1, num2));
+            Assert.AreEqual(7.5f, Calculator.Multiple(num1, num2));
         }
     }
 }
