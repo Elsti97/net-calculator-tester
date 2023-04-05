ffc0c6ce6e9dc6b2524726087029333bf998091d
Elsti <Elsti.Shehati@hotmail.it>
2023-04-06 00:31:30 +0200

    Exercise done

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using net_calculator_tester;
 
+
 namespace CalculatorTest
 {
     public class Tests
@@ -13,21 +14,21 @@ namespace CalculatorTest
         [TestCase(1.5f, 3.1f)]
         public void AddTest(float num1, float num2)
         {
-            Assert.AreEqual(4.6f, Calculator.Add(num1, num2));
+            Assert.That(Calculator.Add(num1, num2), Is.EqualTo(4.6f), "Somma corretta");
         }
 
         [Test]
         [TestCase(5.5f, 3.1f)]
         public void SubtractTest(float num1, float num2)
         {
-            Assert.AreEqual(2.4f, Calculator.Substract(num1, num2));
+            Assert.That(Calculator.Subtract(num1, num2), Is.EqualTo(2.4f), "Sottrazione corretta");
         }
 
         [Test]
         [TestCase(4.5f, 1.5f)]
         public void DivideTest(float num1, float num2)
         {
-            Assert.AreEqual(3f, Calculator.Divide(num1, num2));
+            Assert.That(Calculator.Divide(num1, num2), Is.EqualTo(3f), "Divisione corretta");
         }
 
         [Test]
@@ -41,7 +42,7 @@ namespace CalculatorTest
         [TestCase(2.5f, 3f)]
         public void MultiplyTest(float num1, float num2)
         {
-            Assert.AreEqual(7.5f, Calculator.Multiple(num1, num2));
+            Assert.That(Calculator.Multiply(num1, num2), Is.EqualTo(7.5f), "Moltiplicazione corretta");
         }
     }
 }
